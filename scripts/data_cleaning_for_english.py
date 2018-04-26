e21fe5c611693191f4ea519cb758710540e4acc2
Tomoya Mizumoto <tm0mo.3120@gmail.com>
2018-04-26 15:17:47 +0900

    add cleaning_for_english.py

---
+++ scripts/data_cleaning_for_english.py
@@ -0,0 +1,92 @@
+import sys
+import platform
+import codecs
+from nltk import tokenize
+from joblib import Parallel, delayed
+import mojimoji
+import langdetect
+from langdetect import DetectorFactory
+
+
+# number of parallel processing
+para = 4
+
+# settings for xxx2018 paper
+DetectorFactory.seed = 0
+d_num = 6
+i_num = 6
+#####
+
+def get_lang(text):
+    try:
+        lang = langdetect.detect(text)
+    except:
+        lang = "None"
+    return lang
+
+def check_ascii(text):
+    if text:
+        return max([ord(char) for char in text]) < 128
+    return True
+
+def levenshtein_distance(errs, corrs):
+    errs_len = len(errs)
+    corrs_len = len(corrs)
+    inf = float("inf")
+    dp = [[inf for i in range(corrs_len+1)] for j in range(errs_len+1)]
+    dp[0][0] = 0
+
+    for i in range(-1, errs_len):
+        for j in range(-1, corrs_len):
+            if i == -1 and j == -1:
+                continue
+            elif i >= 0 and j >= 0:
+                if errs[i].lower() == corrs[j].lower():
+                    dp[i+1][j+1] = min(dp[i][j], dp[i][j+1] + 1, dp[i+1][j] + 1)
+                else:
+                    dp[i+1][j+1] = min(dp[i][j] + 1, dp[i][j+1] + 1, dp[i+1][j] + 1)
+            elif i >= 0:
+                dp[i+1][j+1] = dp[i][j+1] + 1
+            elif j >= 0:
+                dp[i+1][j+1] = dp[i+1][j] + 1
+
+    del_num, ins_num = 0, 0
+    while i > 0 and j > 0:
+        dp_val = [dp[i-1][j-1], dp[i-1][j], dp[i][j-1]]
+        min_idx = dp_val.index(min(dp_val))
+        if dp[i][j] == dp[i-1][j-1] and min_idx == 0:
+            i -= 1
+            j -= 1
+            continue
+        elif min_idx == 0:
+            del_num += 1
+            ins_num += 1
+            i -= 1
+            j -= 1
+        elif min_idx == 1:
+            del_num += 1
+            i -= 1
+        else:
+            ins_num += 1
+            j -= 1
+    return del_num, ins_num
+
+def process(text):
+    text = mojimoji.zen_to_han(text.rstrip(), kana=False)
+    lang = get_lang(text)
+    ascii = check_ascii(text)
+    if lang == 'en' and ascii:
+        err_corr = text.split("\t")
+        if len(err_corr) == 2:
+            errs = tokenize.word_tokenize(err_corr[0])
+            corrs = tokenize.word_tokenize(err_corr[1])
+            del_num, ins_num = levenshtein_distance(errs, corrs)
+            if del_num < d_num and ins_num < i_num:
+                print(" ".join(errs) + "\t" + " ".join(corrs))
+
+if __name__ == "__main__":
+    assert platform.python_version_tuple()[0] == '3', 'This program supports only python3'
+    file = sys.argv[1]
+    with codecs.open(file, 'r', encoding='utf8') as f:
+        Parallel(n_jobs=para, verbose=5)([delayed(process)(text) for text in f])
+
